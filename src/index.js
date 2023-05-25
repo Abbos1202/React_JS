1f3b99c558f89414f75d95974d2ba0194f4c77c8
Abbos_Ibrokhimov <abbosibrohimov07@gmail.com>
2023-05-25 09:14:23 +0500

    Fruits card

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Student from './Student.jsx';
+import App from './App.js'
 
-const students = [
-  { id: 1, name: 'Sardor' },
-  { id: 2, name: 'Dilshod' },
-  { id: 3, name: 'Azizbek' },
-  { id: 4, name: 'Firdavs' },
-  { id: 5, name: 'Abbos' },
-  { id: 6, name: 'Sirojiddin' },
-  { id: 7, name: 'Damir' },
-  { id: 8, name: 'Abror' },
-  { id: 9, name: 'Asror' },
-];
+
 
 ReactDOM.render(
   <React.StrictMode>
-    {students.map((value) => {
-      return <Student data={value} />;
-    })}
+     <div>
+      <App />
+     </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
