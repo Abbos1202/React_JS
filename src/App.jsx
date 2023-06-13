d6f9dd906541b86979e8eb65d492f4a32f243f20
Abbos_Ibrokhimov <abbosibrohimov07@gmail.com>
2023-06-13 11:07:48 +0500

    list, key prop, value.id vs index

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 import React, { Component } from "react";
-import SignIn from "./SignIn";
-import SignUp from "./SignUp";
+
+const list = [
+  { id: 1, title: "Webbrain Academy" },
+  { id: 2, title: "Digital One" },
+  { id: 3, title: "Top Class Academy" },
+  { id: 4, title: "Share Academy" },
+  { id: 5, title: "WBA Academy" },
+];
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       active: true,
+      list: list,
     };
   }
   render() {
+    const onDelete = (id) => {
+      console.log(id);
+      let filtered = this.state.list.filter((value) => value.id !== id)
+      this.setState({list: filtered})
+    }
     return (
       <div>
-        <SignIn />
-        <SignUp />
+        <h1>List</h1>
+        {this.state.list.map((value, index) => (
+          <div>
+            <h1>
+              {index + 1} {value.id} {value.title} <button onClick={()=> onDelete(value.id)}>delete</button>
+            </h1>
+          </div>
+        ))}
       </div>
     );
   }
