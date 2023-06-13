2c0e5b8494e9538017dbdf80fd57c65516c27bd9
Abbos_Ibrokhimov <abbosibrohimov07@gmail.com>
2023-06-13 09:40:42 +0500

    Form

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Render from './Render';
+import ReactDOM from 'react-dom/client';
+// import App from './App';
+import "./index.css";
+import LoginContol from './LoginContol';
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Render />
-  </React.StrictMode>,
-  document.getElementById('root')
+    {/* <App /> */}
+    <LoginContol />
+  </React.StrictMode>
 );
