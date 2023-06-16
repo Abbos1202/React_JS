f734a58bff7c6bdcfa5428669fe922933b921ba4
Abbos_Ibrokhimov <abbosibrohimov07@gmail.com>
2023-06-16 20:52:03 +0500

    YouTube clone

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import YouTube from './YouTube';
+
+
+
+const App = () => {
+  return (
+    <div>
+      <YouTube />
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
-import Render from './Render';
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Render />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
