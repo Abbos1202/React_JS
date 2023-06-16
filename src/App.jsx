13846b54a2c086353acdb5cfc690e749b0e11711
Abbos_Ibrokhimov <abbosibrohimov07@gmail.com>
2023-06-16 19:41:48 +0500

    npx create-react-app, babel, single page application, jsx, react app settings, src

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,11 @@
-import React, { Component } from "react";
+import React from 'react';
 
-const list = [
-  { id: 1, title: "Webbrain Academy" },
-  { id: 2, title: "Digital One" },
-  { id: 3, title: "Top Class Academy" },
-  { id: 4, title: "Share Academy" },
-  { id: 5, title: "WBA Academy" },
-];
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: true,
-      list: list,
-    };
-  }
-  render() {
-    const onDelete = (id) => {
-      console.log(id);
-      let filtered = this.state.list.filter((value) => value.id !== id)
-      this.setState({list: filtered})
-    }
-    return (
-      <div>
-        <h1>List</h1>
-        {this.state.list.map((value, index) => (
-          <div>
-            <h1>
-              {index + 1} {value.id} {value.title} <button onClick={()=> onDelete(value.id)}>delete</button>
-            </h1>
-          </div>
-        ))}
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      hi again
+    </div>
+  )
 }
+
+export default App;
